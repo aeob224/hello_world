31840e5c9ff5577984fa7248cb22170e399293de
aeob224 <aidan.obrien823@gmail.com>
2025-09-09 10:10:22 -0400

    Changed  plot code to make points steel blue.

--- github_pull_practice.R
+++ github_pull_practice.R
@@ -1,9 +1,11 @@
 library(EVR628tools)
 library(ggplot2)
+
 dat <- EVR628tools::data_lionfish
 ggplot(data = dat,
-mapping = aes(x = total_length_mm,
-y = total_weight_gr))+
-geom_point()+
-labs(x = "Total Length (mm)",
-     y = "Weight (g)")
+       mapping = aes(x = total_length_mm,
+                     y = total_weight_gr))+
+  geom_point(fill = "steelblue", shape = 21, size = 2)+
+  labs(x = "Total Length (mm)",
+       y = "Weight (g)")
+
